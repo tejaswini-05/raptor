490298af463361ab8f3e6bb18899072d4d28262f
tejaswini-05 <tejqswini.pallamreddy57@gmail.com>
2019-09-07 17:39:03 +0530

    Second Try

---
+++ outputoperator.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace outputoperator
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+      
+         Console.WriteLine("tejaswini");
+      }
+   }
+}
